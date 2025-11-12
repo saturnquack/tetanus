b2998fbe7bdba9ac3d6042149229250b6244ce50
saturn <157832049+saturnquack@users.noreply.github.com>
2025-11-12 10:48:01 -0500

    style chance

--- tetris.js
+++ tetris.js
@@ -184,7 +184,7 @@ function Tetris(controller) {
 
 window.onload = function () {
     loadGameControls();
-if(x == 1) {
+if(styleVar == 1) {
     jaws.assets.add('media/blueblock.png');
     jaws.assets.add('media/cyanblock.png');
     jaws.assets.add('media/greenblock.png');
